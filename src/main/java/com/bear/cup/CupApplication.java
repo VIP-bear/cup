573282ddb76555da7b5ea570b94310899687834d
VIP-bear <1907464379@qq.com>
2020-03-15 20:45:21 +0800

    创建项目

---
+++ src/main/java/com/bear/cup/CupApplication.java
@@ -0,0 +1,13 @@
+package com.bear.cup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bear/cup/CupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bear.cup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
